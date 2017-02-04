247dba38db021b4dde4542bbcc9648421d0b0ee8
DogeSWL <lusen.w1387@gmail.com>
2017-02-03 20:44:09 -0800

    finished Ninja Cat

--- jQuery_WF/Ninja_Cat/jScript.js
+++ jQuery_WF/Ninja_Cat/jScript.js
@@ -12,6 +12,16 @@ $(document).ready(function() {
     }
   };
 
+  function chngPic() {
+    var ranNum1 = Math.floor(Math.random() * 2)+1;
+    var ranNum2 = Math.floor(Math.random() * 5);
+
+    if(ranNum1 == 2) {
+      return "img/cat"+ranNum2+".png";
+    }else {
+      return "img/ninja"+ranNum2+".png";
+    }  }
+
   //creates 5 div boxes and append it to container div
   //genPic function adds img into div box as it is created
   for(var i=0; i<5; i++) {
@@ -23,6 +33,6 @@ $(document).ready(function() {
 
   //on click changes img *problem: only able to change once*
   $('img').click(function() {
-    $(this).replaceWith(genPic());
+    $(this).attr('src', chngPic() );
   });
 });
