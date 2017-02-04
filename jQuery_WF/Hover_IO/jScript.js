546a6d611991da6ddee0bb67fcf4d08a83f69e6f
DogeSWL <lusen.w1387@gmail.com>
2017-02-03 21:23:50 -0800

    finished Hover In/Out

--- jQuery_WF/Hover_IO/jScript.js
+++ jQuery_WF/Hover_IO/jScript.js
@@ -1,30 +1,10 @@
-function numCheck(num) {
-  if(num==1) {
-    return "<img src='img/git4.jpg'>";
-  }
-  if(num==2) {
-    return "<img src='img/git5.jpg'>";
-  }
-  if(num==3) {
-    return "<img src='img/git6.png'>";
-  }
-  if(num==4) {
-    return "<img src='img/git1.png'>";
-  }
-  if(num==5) {
-    return "<img src='img/git2.png'>";
-  }
-  if(num==6) {
-    return "<img src='img/git3.png'>";
-  }
-};
-
 $(document).ready(function() {
 
   $('img').hover(function() {
-    $(this).replaceWith(numCheck($(this).attr('data-alt-src')));
-  }, function() {
-    $(this).replaceWith(numCheck($(this).attr('data-alt-src')));
-  });
+    $(this).attr('src', $(this).attr('alt-src'));
+    }, function() {
+    $(this).attr('src', $(this).attr('default-src')) ;
+    }
+  );
 
 });
