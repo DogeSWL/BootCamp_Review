1c71687200fcead4810ed661fc83cf5fa6707510
DogeSWL <lusen.w1387@gmail.com>
2017-02-28 17:17:32 -0800

    completed Contact_Card

--- jQuery_WF/Contact_Card/jQScript.js
+++ jQuery_WF/Contact_Card/jQScript.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-  $(".cardBox").on("click", function() {
+
+  $("form").submit(function() {
+    $(".rightContainer").append('<div class=cardBox><h1>'+
+    addForm.firstName.value+' '+addForm.lastName.value+'</h1><h4>Click for description!</h4><h3>'+
+    addForm.desBox.value+'</h3></div>');
+
+    //adds event listener to element
+    $(".cardBox").on("click", function() {
+      $(this).children().toggle();;
+    });
+
+    //reset the form after submitting
+    $("form")[0].reset();
+
+    return false;
+  });
+
+  //click on cardBox class will cause element to toggle
+  $(".cardBox").click(function() {
     $(this).children().toggle();;
   });
+
 });
