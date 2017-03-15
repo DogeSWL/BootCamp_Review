188667e3bc46ca65d80227a6e44bf836683c6897
SenWLu <lusen.w1387@gmail.com>
2017-03-15 00:04:11 -0700

    completed Bike

--- Python_OOP/Bike.py
+++ Python_OOP/Bike.py
@@ -1,7 +1,34 @@
 class Bike(object):
-    def __init__(self, price, max_speed, miles):
+    def __init__(self, price, max_speed):
         self.price = price
         self.max_speed = max_speed
-        self.miles = miles
+        self.miles = 0
 
-bikeA = Bike
+    def displayInfo(self):
+        print("Price of bike is:",self.price)
+        print("Max speed of bike is: %smph" % (self.max_speed))
+        print("Total miles:", self.miles)
+
+    def ride(self):
+        self.miles += 10
+
+    def reverse(self):
+        if (self.miles - 5) < 0:
+            self.miles = 0
+        else:
+            self.miles -= 5
+
+# bikeA = Bike(200, 120)
+# bikeA.ride()
+# bikeA.ride()
+# bikeA.ride()
+# bikeA.reverse()
+# bikeA.displayInfo()
+
+bikeB = Bike(50, 80)
+bikeB.ride()
+bikeB.reverse()
+bikeB.reverse()
+bikeB.reverse()
+bikeB.reverse()
+bikeB.displayInfo()
